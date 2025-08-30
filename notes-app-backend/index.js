01565f8d83932f352d70762d5ecb48d832afcfd5
harshneyy <harshitverma576@gmail.com>
2025-08-30 22:55:45 +0530

    Update CORS for production deployment

--- notes-app-backend/index.js
+++ notes-app-backend/index.js
@@ -10,7 +10,16 @@ connectDB();
 const app = express();
 
 // --- MIDDLEWARE ---
-app.use(cors()); // Enable CORS for all routes
+
+
+
+const corsOptions = {
+  origin: 'https://notes-app-five-theta.vercel.app/', // <-- PASTE YOUR VERCEL URL HERE
+  optionsSuccessStatus: 200 // For legacy browser support
+};
+
+app.use(cors(corsOptions)); // Use the new options here
+
 app.use(express.json()); // This is crucial! It allows our app to accept JSON.
 
 // --- DEFINE ROUTES ---
